6ef92af96d8afba2a0f7bc22b443eee7802b7e26
Alejandro Hussein <alejandrohussein1@gmail.com>
2023-08-23 09:22:23 -0500

    Enums

--- app.js
+++ app.js
@@ -8,9 +8,30 @@ var person = {
         number: 403
     }
 };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 5] = "ADMIN";
+    Role[Role["READ_ONLY"] = 6] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 7] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var anotherPerson = {
+    name: 'Ian',
+    age: 11,
+    hobbies: ['Games', 'Movies'],
+    role: Role.ADMIN,
+    address: {
+        street: 'Liberty',
+        number: 403
+    }
+};
 var favActivities;
 console.log(person.name);
+console.log(anotherPerson);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (anotherPerson.role === Role.ADMIN) {
+    console.log('the role is admin');
 }
--- app.ts
+++ app.ts
@@ -19,10 +19,37 @@ const person: {
   }
 }
 
+enum Role {ADMIN = 5, READ_ONLY, AUTHOR};
+
+const anotherPerson: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: Role;
+  address: {
+    street: string;
+    number: number;
+  }
+}= {
+  name: 'Ian',
+  age: 11,
+  hobbies: ['Games', 'Movies'],
+  role: Role.ADMIN,
+  address: {
+    street: 'Liberty',
+    number: 403 
+}
+}
+
 let favActivities: string[];
 
 console.log(person.name)
+console.log(anotherPerson);
 
 for(const hobby of person.hobbies){
   console.log(hobby.toUpperCase())
+}
+
+if(anotherPerson.role === Role.ADMIN){
+  console.log('the role is admin');
 }
