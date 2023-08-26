4cd24b93db89f9cd3464416776da6a1629d42c5f
Alejandro Hussein <alejandrohussein1@gmail.com>
2023-08-26 14:54:21 -0500

    Private & Public Access Modifiers

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,24 @@
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = []; // private property only access inside the class
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log("Department: " + this.name);
     };
+    Department.prototype.addEmployees = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInfo = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department("Accounting");
+accounting.addEmployees('Ale');
+accounting.addEmployees('Ian');
 accounting.describe();
-var accountingCopy = { name: "DUMMY", describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInfo();
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// accountingCopy.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;               // public property can access outside the class
+  private employees: string[]= [];  // private property only access inside the class
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,26 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+  addEmployees(employee: string){
+    this.employees.push(employee)
+  }
+  
+  printEmployeeInfo(){
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
+
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployees('Ale');
+accounting.addEmployees('Ian');
+
 accounting.describe();
+accounting.printEmployeeInfo();
 
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
 
-accountingCopy.describe();
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+// accountingCopy.describe();
