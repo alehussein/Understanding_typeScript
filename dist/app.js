420bd5e51c0ed4c199dd79896a3305073eccbd2e
Alejandro Hussein <alejandrohussein1@gmail.com>
2023-08-28 16:31:31 -0500

    Types Guards

--- dist/app.js
+++ dist/app.js
@@ -3,3 +3,48 @@ var e1 = {
     privileges: ["create-server"],
     startDate: new Date(),
 };
+function add(n1, n2) {
+    if (typeof n1 === "number" && typeof n2 === "number") {
+        return n1 + n2;
+    }
+    return n1.toString() + n2.toString();
+}
+function printEmployeeInfo(emp) {
+    console.log("Name: ", emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: ", emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("StartDate: ", emp.startDate);
+    }
+}
+printEmployeeInfo(e1);
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.drive = function () {
+        console.log("Driving ...");
+    };
+    return Car;
+}());
+var Truck = /** @class */ (function () {
+    function Truck() {
+    }
+    Truck.prototype.drive = function () {
+        console.log("Driving a truck");
+    };
+    Truck.prototype.loadCargo = function (amount) {
+        console.log("loading cargo " + amount);
+    };
+    return Truck;
+}());
+var v1 = new Car();
+var v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- src/app.ts
+++ src/app.ts
@@ -20,3 +20,54 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(n1: Combinable, n2: Combinable) {
+  if (typeof n1 === "number" && typeof n2 === "number") {
+    return n1 + n2;
+  }
+  return n1.toString() + n2.toString();
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+  console.log("Name: ", emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: ", emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("StartDate: ", emp.startDate);
+  }
+}
+
+printEmployeeInfo(e1);
+
+class Car {
+  drive() {
+    console.log("Driving ...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck");
+  }
+
+  loadCargo(amount: number) {
+    console.log("loading cargo " + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+useVehicle(v1);
+useVehicle(v2);
