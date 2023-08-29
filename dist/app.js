346c36a0ec397f2cbbfe00bd3eda381dd1a2fe39
Alejandro Hussein <alejandrohussein1@gmail.com>
2023-08-29 14:28:08 -0500

    Generic Classes

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 //Generics
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var names = [];
 // names[0].split(' ');
 var promise = new Promise(function (resolve, reject) {
@@ -37,3 +46,43 @@ function extractAndConvert(obj, key) {
     return "value: " + obj[key];
 }
 console.log(extractAndConvert({ name: "ALE" }, "name"));
+// Generic Classes
+var DataStorage = /** @class */ (function () {
+    function DataStorage() {
+        this.data = [];
+    }
+    DataStorage.prototype.addItem = function (item) {
+        this.data.push(item);
+    };
+    DataStorage.prototype.removeItem = function (item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    DataStorage.prototype.getItem = function () {
+        return __spreadArray([], this.data, true);
+    };
+    return DataStorage;
+}());
+var textStorage = new DataStorage();
+textStorage.addItem("Ale");
+textStorage.addItem("Ian");
+textStorage.addItem("Monte");
+console.log(textStorage.getItem());
+var numberStorage = new DataStorage();
+numberStorage.addItem(41);
+numberStorage.addItem(11);
+numberStorage.addItem(76);
+console.log(numberStorage.getItem());
+function createCourseGoal(title, description, date) {
+    var courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUnit = date;
+    return courseGoal;
+    // return { title: title, description: description, completeUnit: date };
+}
+var anotherNames = ["Max", "Sports"];
+// anotherNames.push("Manu");
+// anotherNames.pop()
--- src/app.ts
+++ src/app.ts
@@ -58,3 +58,62 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: "ALE" }, "name"));
+
+// Generic Classes
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItem() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Ale");
+textStorage.addItem("Ian");
+textStorage.addItem("Monte");
+console.log(textStorage.getItem());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(41);
+numberStorage.addItem(11);
+numberStorage.addItem(76);
+console.log(numberStorage.getItem());
+
+/// utility types
+
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUnit: Date;
+}
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUnit = date;
+  return courseGoal as CourseGoal;
+  // return { title: title, description: description, completeUnit: date };
+}
+
+const anotherNames: Readonly<string[]> = ["Max", "Sports"];
+// anotherNames.push("Manu");
+// anotherNames.pop()
+
